460817e4a38d8f65daf01ef801ebe553f9c19e3c
Dalip Singh <dsna10@myseneca.ca>
2025-01-11 00:06:28 -0500

    Completed lab

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program performs basic math operations using integers.'''
+
+# Objects to store integers
+x = 10
+y = 2
+z = 5
+
+# Perform the calculation and print the result
+print(f"{x} + {y} * {z} = {x + y * z}")
