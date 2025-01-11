c4dff15f562ef92dbf6c131437b2a3f5a0cf069f
Dalip Singh <dsna10@myseneca.ca>
2025-01-10 23:51:43 -0500

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Description: This program will display Isaac's age.'''
+
+# Object to store name
+name = 'Isaac'
+
+# Object to store age
+age = 72
+
+# Print the message
+print(name + ' is ' + str(age) + ' years old!')
