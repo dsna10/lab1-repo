3ffe8b6495ee33d33d434c7a9efc485ec9d4d437
Dalip Singh <dsna10@myseneca.ca>
2025-01-10 23:50:47 -0500

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This program will ask Isaac about their age.'''
+
+# Object to store a name
+name = 'Isaac'
+
+# Print the message
+print('How old are you ' + name + '?')
